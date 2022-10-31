73c12b6ee3f299e9d02629f0bee1a9cbb2fdffec
Riatte <95982898+Riatte@users.noreply.github.com>
2022-10-31 16:31:53 +0000

    Deploying to gh-pages from @ Riatte/team10-group-project@ac0bdfb036f8b1fd67d68c9e09041f5d229531c0 🚀

---
+++ index.1cea9a02.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".slick-prev"),o=document.querySelector(".slick-next");$(document).ready((function(){$(".slider").slick({autoplay:!0,autoplaySpeed:5e3,pauseOnFocus:!1,prevArrow:"".concat(e.outerHTML),nextArrow:"".concat(o.outerHTML)})})),e.remove(),o.remove()}();
+//# sourceMappingURL=index.1cea9a02.js.map
