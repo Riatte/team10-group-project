caaaf176034123a151e51e790b9b2bede4a94667
Riatte <95982898+Riatte@users.noreply.github.com>
2022-10-31 19:38:47 +0000

    Deploying to gh-pages from @ Riatte/team10-group-project@94c1eaef7c62987988767ff0a6a02243224fc76a 🚀

--- index.6a879100.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector(".slick-prev"),r=document.querySelector(".slick-next");$(document).ready((function(){$(".slider").slick({autoplay:!0,autoplaySpeed:5e3,pauseOnFocus:!1,prevArrow:`${e.outerHTML}`,nextArrow:`${r.outerHTML}`})})),e.remove(),r.remove();
-//# sourceMappingURL=index.6a879100.js.map
