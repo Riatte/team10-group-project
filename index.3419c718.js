238ed4bbe14e4e5a24c22d03037081bb8cfa90c0
Riatte <95982898+Riatte@users.noreply.github.com>
2022-11-01 19:32:20 +0000

    Deploying to gh-pages from @ Riatte/team10-group-project@b70d2371a0a2e901dbe628d1a275fb40d1d6e4f5 🚀

---
+++ index.3419c718.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("a").on("click",(function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},800,(function(){window.location.hash=t}))}}))}));
+//# sourceMappingURL=index.3419c718.js.map
