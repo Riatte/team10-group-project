0592e30d2143f1c6781d7e04bc1c63cdcb56adde
Viktoria <vik25@i.ua>
2022-11-04 22:53:31 +0100

    fix bugs

--- src/js/forma.js
+++ src/js/forma.js
@@ -3,7 +3,7 @@
   const sendBtn = document.querySelector(".order__btn-send");
 
   orderInput.addEventListener("input", (event) => {
-    if (event.target.value.length > 1)
+    if (event.target.value.length === 0)
       return sendBtn.classList.remove("validForm");
 
     sendBtn.classList.add("validForm");
