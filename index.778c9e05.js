5f708d823ab6b0bcfde80112d9bacc8540dd054e
Riatte <95982898+Riatte@users.noreply.github.com>
2022-11-05 11:06:45 +0000

    Deploying to gh-pages from @ Riatte/team10-group-project@45c892eadb2e5178d19a5451ef842a382968affa 🚀

---
+++ index.778c9e05.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector(".order__input"),t=document.querySelector(".order__btn-send");e.addEventListener("input",(e=>{if(0===e.target.value.length)return t.classList.remove("validForm");t.classList.add("validForm")}))})();
+//# sourceMappingURL=index.778c9e05.js.map
---
+++ index.cc7fd102.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector(".order__input"),t=document.querySelector(".order__btn-send"),e.addEventListener("input",(function(e){if(0===e.target.value.length)return t.classList.remove("validForm");t.classList.add("validForm")}))}();
+//# sourceMappingURL=index.cc7fd102.js.map
